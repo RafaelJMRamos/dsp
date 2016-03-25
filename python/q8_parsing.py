aa764aa3e35c0b3e7e6f363d541711c428d8ef85
RafaelJMRamos <rafaeljmramos@gmail.com>
2016-03-25 13:04:24 -0700

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -9,11 +9,21 @@
 
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+def read_data(data):    
+  with open(data, 'r') as f:
+    data = [row for row in csv.reader(f.read().splitlines())]
+  return data
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+data = footall.csv    
+    
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+def get_min_score_difference(self, parsed_data):
+  parsed_data.pop(0)
+  goals = [x[5] for x in parsed_data]
+  goals_allowed = [x[6] for x in parsed_data]
+  return min([float(x) - float(y) for x, y in zip(goals, goals_allowed)])
+
+
+def get_team(self, index_value, parsed_data):
+  teams = [x[0] for x in parsed_data]
+  return teams[index_value]

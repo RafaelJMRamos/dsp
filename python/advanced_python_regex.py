9e2aa649297ef262344555ce79166687a869e5d6
RafaelJMRamos <rafaeljmramos@gmail.com>
2016-03-25 15:53:29 -0700

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,20 @@
-PLACE YOUR CODE HERE
+import csv
+def read_data(data):
+  with open(data, 'r') as f:
+    data =[row for row in csv.reader(f.read().splitlines())]
+  return data
+  
+data = Desktop/faculty.csv
+
+fac = read_data(data)
+
+degrees = [x[1] for x in fac]
+degree_unique = set(degree)
+print (degree_unique)
+#8 different degrees
+degrees.count('Ph.D')
+degrees.count('PhD')
+degrees.count('ScD')
+degrees.count('Sc.D')
+
+

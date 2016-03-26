23806f25baa55bb22edd4d29df6d30f5d988d5d6
RafaelJMRamos <rafaeljmramos@gmail.com>
2016-03-25 23:40:20 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,34 @@
-PLACE YOUR CODE HERE
+#q6
+
+def read_data(data):
+  with open(data, 'r') as f:
+    data =[row for row in csv.reader(f.read().splitlines())]
+  return data
+  
+data = Desktop/faculty.csv
+
+fac = read_data(data)
+fac = fac[1:]
+nonames = [x[1:] for x in fac]
+names = [x[0] for x in fac]
+lastname = [x.split(' ')[-1] for x in names]
+
+tuplast = tuple(lastname)
+from collections import defaultdict
+faculty_dict =defaultdict(list)
+
+for i,j in zip(tuplast,nonames):
+  faculty_dict[i].append(j)
+print (faculty_dict)  
+#q7
+firstname = [x.split(' ')[0] for x in names]
+newname = list(zip(firstname,lastname))
+professor_dict = dict(zip(newname,nonames))
+print (professor_dict)
+#q8
+order = sorted(professor_dict, key = lambda x:x[1])
+for key in order:
+  print(key, professor_dict[key])
+
+
+

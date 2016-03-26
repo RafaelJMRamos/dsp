6fd350ed1189b77ca1fed6809f8c065f1917fe96
RafaelJMRamos <rafaeljmramos@gmail.com>
2016-03-25 17:00:56 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,8 @@
-PLACE YOUR CODE HERE
+#removing header
+emails = emails [:1]
+
+csvfile = 'emails.csv'
+with open(csvfile, 'w') as output:
+  writer=csv.writer(output, lineterminator='\n')
+  for val in emails:
+    writer.writerow([val])

2ec58b0d24fc35d7b0fe6c307acda685435f2061
RafaelJMRamos <rafaeljmramos@gmail.com>
2016-03-25 23:55:58 -0700

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,3 +1,4 @@
+#q1
 import csv
 def read_data(data):
   with open(data, 'r') as f:
@@ -9,24 +10,31 @@ data = Desktop/faculty.csv
 fac = read_data(data)
 
 degrees = [x[1] for x in fac]
-degree_unique = set(degree[1:])
+no_periods= [x.replace('.','') for x in degrees]
+no_space= [x.replace(' ','') for x in no_periods]
+degree_unique = set(no_space[1:])
 print (degree_unique)
 #8 different degrees
-degrees.count('Ph.D')
-degrees.count('PhD')
-degrees.count('ScD')
-degrees.count('Sc.D')
+no_space.count('PhD')
+no_space.count('PhDScD')
+no_space.count('ScD')
+no_space.count('MDMPHPhD')
+no_space.count('BSEdMSPhD')
+no_space.count('JDMAMPHMSPhD')
 
+#q2
 titles = [x[2] for x in fac]
 title_unique = set(titles[1:])
 titles.count('Associate Professor of Biostatistics')
 titles.count('Assistant Professor of Biostatistics')
 titles.count('Professor of Biostatistics')
 
+#q3
 emails = [x[3] for x in fac]  
 headerless_emails=emails[1:]  
 print(headerless_emails)
 
+#q4
 domains = [x.split('@',2)[-1] for x in emails]
 uniquedomains=list(set(domains[1:]))
 
